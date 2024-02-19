0da97cd85d430409c34155d93da81f78f64e77a8
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-02-19 14:04:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const parseCode = (str) => {
 	const regex = /^(.*?)000(.*?)000(\d+)$/;
   
   // Use the regular expression to match the input string
-  const match = input.match(regex);
+  const match = str.match(regex);
 
   if (match) {
     // Extract the captured groups and create the result object
-    const [, firstName, lastName, id] = match;
+    const [firstName, lastName, id] = match;
     return { firstName, lastName, id };
   } else {
     // Return null or an appropriate value if the input doesn't match the expected format
